4298a12ed06e0a4c3a7da825f36bec371e8068b4
Cihan KAFADAR <80070465+Cihaan@users.noreply.github.com>
2022-01-12 20:28:27 +0100

    attaque player + attaque monster + affichage point de vie player et monster

---
+++ app.js
@@ -0,0 +1,37 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      monsterHealth: 100,
+      playerHealth: 100,
+    };
+  },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + "%" };
+    },
+  },
+  methods: {
+    attackMonster() {
+      console.log(this.monsterHealth);
+      console.log(this.playerHealth);
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 15);
+      this.playerHealth -= attackValue;
+      console.log(this.monsterHealth);
+      console.log(this.playerHealth);
+    },
+  },
+});
+
+app.mount("#game");
